60fa57667193620f019af1030499ec09c5377acd
Josh Villaluz <154228901+jvlzloona666@users.noreply.github.com>
2024-03-11 00:09:56 +0800

    Added javascript. Program is functioning
    Added javascript. Program is functioning. Updated
    README and added program screenshots.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rock-paper-scissors
 Javascript Practice - Rock, Paper, Scissors
+![Prompt](pic1.png)
+![Output](pic2.png)
+Basic Rock, Paper, Scissors game in Javascript.
+Written using if-else statements. Can be optimized.
---
+++ main.js
@@ -0,0 +1,72 @@
+function getComputerChoice() {
+    let x = Math.floor(Math.random() * 3) + 1;
+    if (x === 1) {
+        return 1;
+    } else if (x === 2) {
+        return 2;
+    } else {
+        return 3;
+    }
+}
+
+function computerPick(pick) {
+    let x = pick;
+    if (x === 1) {
+        console.log("Computer picked rock.");
+    } else if (x === 2) {
+        console.log("Computer picked paper.");
+    } else {
+        console.log("Computer picked scissors.");
+    }
+
+}
+
+function playRound(computerSelection) {
+    let answer = prompt("Rock, paper or scissors?").toLowerCase();
+    let y;
+    switch(answer) {
+        case "rock":
+            console.log("You picked rock.");
+            y = 1;
+            break;
+        case "paper":
+            console.log("You picked paper.");
+            y = 2;
+            break;
+        case "scissors":
+            console.log("You picked scissors.");
+            y = 3;
+            break;
+        default:
+            console.log("Incorrect input.");
+            y = 0;
+    }
+    let comp = computerSelection;
+    // 1. rock 2. paper 3. scissors
+    if (comp === 1 && y === 1 ) {
+        return ("You both picked rock. It is a tie.");
+    } else if (comp === 1 && y == 2) {
+        return ("You win. Paper defeats rock");
+    } else if (comp === 1 && y === 3) {
+        return ("You lose. Rock defeats scissors");
+    } else if (comp === 2 && y === 1) {
+        return ("You lose. Paper defeats rock");
+    } else if (comp === 2 && y === 2) {
+        return ("You both picked paper. It is a tie.");
+    } else if (comp === 2 && y === 3) {
+        return ("You win. Scissors defeats paper");
+    } else if (comp === 3 && y === 1) {
+        return ("You win. Rock defeats scissors");
+    } else if (comp === 3 && y === 2) {
+        return ("You lose. Scissors defeats paper");
+    } else if (comp === 3 && y === 3) {
+        return ("You both picked scissors. It is a tie.");
+    } else {
+        return ("Invalid input. Please enter either rock, paper, or scissors.");
+    }
+}
+
+
+const computerSelection = getComputerChoice();
+console.log(playRound(computerSelection));
+computerPick(computerSelection);
