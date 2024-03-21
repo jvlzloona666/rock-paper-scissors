c75064dcf2f9e76dde7da43aa3a8abfb83bbe4c6
Josh <154228901+jvlzloona666@users.noreply.github.com>
2024-03-21 21:45:03 +0800

    Finished game but needs refinement.

--- main.js
+++ main.js
@@ -9,6 +9,96 @@ function getComputerChoice() {
     }
 }
 
+function getPlayerChoice(callback) {
+    let isRock = document.getElementById('rock');
+    isRock.onclick = function() {
+        callback(1);
+    }
+
+    let isPaper = document.getElementById('paper');
+    isPaper.onclick = function() {
+        callback(2);
+    }
+
+    let isScissor = document.getElementById('scissors');
+    isScissor.onclick = function() {
+        callback(3);
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(computerSelection, playerSelection) {
+    let answer = playerSelection;
+    let comp = computerSelection;
+  
+    
+    switch(answer) {
+        case 1:
+            console.log("You picked rock.");
+            y = 1;
+            break;
+        case 2:
+            console.log("You picked paper.");
+            y = 2;
+            break;
+        case 3:
+            console.log("You picked scissors.");
+            y = 3;
+            break;
+        default:
+            console.log("Incorrect input.");
+            y = 0;
+    }
+    let headerMessage = document.getElementById('header'); 
+  
+    // 1. rock 2. paper 3. scissors
+    if (comp === 1 && y === 1 ) {
+        headerMessage.innerHTML = "You both picked rock. It is a tie.";
+    } else if (comp === 1 && y == 2) {
+        playerScore++;
+        headerMessage.innerHTML = "You win. Paper defeats rock";
+   
+    } else if (comp === 1 && y === 3) {
+        computerScore++;
+        headerMessage.innerHTML =  "You lose. Rock defeats scissors";
+   
+    } else if (comp === 2 && y === 1) {
+        computerScore++;
+        headerMessage.innerHTML = "You lose. Paper defeats rock";
+   
+    } else if (comp === 2 && y === 2) {
+        headerMessage.innerHTML = "You both picked paper. It is a tie.";
+   
+    } else if (comp === 2 && y === 3) {
+        playerScore++;
+        headerMessage.innerHTML =  "You win. Scissors defeats paper";
+        
+   
+    } else if (comp === 3 && y === 1) {
+        playerScore++;
+        headerMessage.innerHTML = "You win. Rock defeats scissors";
+   
+    } else if (comp === 3 && y === 2) {
+        computerScore++;
+        headerMessage.innerHTML = "You lose. Scissors defeats paper";
+   
+    } else if (comp === 3 && y === 3) {
+        headerMessage.innerHTML = "You both picked scissors. It is a tie.";
+   
+    } else {
+        headerMessage.innerHTML = "Invalid input. Please enter either rock, paper, or scissors.";
+    }
+
+    let scoreElement = document.getElementById('score'); 
+    scoreElement.innerHTML = playerScore;
+
+    let compScoreElement = document.getElementById('com-score'); 
+    compScoreElement.innerHTML = computerScore;
+
+}
+
 function computerPick(pick) {
     let x = pick;
     if (x === 1) {
@@ -18,56 +108,17 @@ function computerPick(pick) {
     } else {
         console.log("Computer picked scissors.");
     }
-
-}
-
-function playRound(computerSelection) {
-    let answer = document.getElementById('rock');
-    console.log(answer);
-    let y;
-    switch(answer) {
-        case "rock":
-            console.log("You picked rock.");
-            y = 1;
-            break;
-        case "paper":
-            console.log("You picked paper.");
-            y = 2;
-            break;
-        case "scissors":
-            console.log("You picked scissors.");
-            y = 3;
-            break;
-        default:
-            console.log("Incorrect input.");
-            y = 0;
-    }
-    let comp = computerSelection;
-    // 1. rock 2. paper 3. scissors
-    if (comp === 1 && y === 1 ) {
-        return ("You both picked rock. It is a tie.");
-    } else if (comp === 1 && y == 2) {
-        return ("You win. Paper defeats rock");
-    } else if (comp === 1 && y === 3) {
-        return ("You lose. Rock defeats scissors");
-    } else if (comp === 2 && y === 1) {
-        return ("You lose. Paper defeats rock");
-    } else if (comp === 2 && y === 2) {
-        return ("You both picked paper. It is a tie.");
-    } else if (comp === 2 && y === 3) {
-        return ("You win. Scissors defeats paper");
-    } else if (comp === 3 && y === 1) {
-        return ("You win. Rock defeats scissors");
-    } else if (comp === 3 && y === 2) {
-        return ("You lose. Scissors defeats paper");
-    } else if (comp === 3 && y === 3) {
-        return ("You both picked scissors. It is a tie.");
-    } else {
-        return ("Invalid input. Please enter either rock, paper, or scissors.");
-    }
 }
 
 
-const computerSelection = getComputerChoice();
-console.log(playRound(computerSelection));
-computerPick(computerSelection);
+function main() {
+    getPlayerChoice(function(player) {
+        let computer = getComputerChoice();
+        computerPick(computer);
+        playRound(computer, player);
+    });
+}
+
+
+
+main();
